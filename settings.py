08ad8735600381721a6c9ea592e02f6ce96ff856
wael26214-blip <wael26214@gmail.com>
2026-08-27 18:42:20 +0300

    Add item properties to settings
    Added item properties including width, height, speed, and spawn interval.

--- settings.py
+++ settings.py
@@ -4,3 +4,8 @@ class Settings:
         self.SCREEN_HEIGHT = 800
         self.NUM_LANES = 3
         self.LANE_WIDTH = (int(self.SCREEN_WIDTH / self.NUM_LANES))-71
+        # Item (Obstacle / Nitro) Properties
+        self.ITEM_WIDTH = 40
+        self.ITEM_HEIGHT = 40
+        self.ITEM_SPEED = 5
+        self.SPAWN_INTERVAL = 60  # Frames between each new spawn (FPS)
